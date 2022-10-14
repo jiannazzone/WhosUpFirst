21a3d2db92fa4f6ee7dc6d319677668bcefc2aa2
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-10-14 14:22:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+(async () => {
+    const prompts = loadPrompts().then( x => {return x});
+    console.log(prompts); // #1
+})();
+
+async function loadPrompts() {
+    try {
+        let response = await fetch('player_prompts.json');
+        let prompts = await response.json();
+        console.log(prompts); // #2
+        return prompts;
+    } catch (error) {
+        console.log(error);
+    }
+}
