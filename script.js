cc78324bd3eb7460f169a210176629f1eae1bc2f
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2022-10-14 14:45:39 -0400

    2: Add event listener for "something funny" button

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-(async () => {
-    const prompts = loadPrompts().then( data => {return data});
-    console.log(prompts); // #1
-})();
+function main() {
+    const funnyButton = document.getElementById('funny-button');
+    // console.log(funnyButton);
+    funnyButton.addEventListener('click', function() {
+        randomPrompt('funny');
+    });
+}
 
 async function loadPrompts() {
     try {
         let response = await fetch('player_prompts.json');
         let prompts = await response.json();
-        console.log(prompts); // #2
         return prompts;
     } catch (error) {
         console.log(error);
     }
+}
+
+async function randomPrompt(category) {
+    const prompts = await loadPrompts().then( data => {return data});
+    const thisCategoryPrompts = prompts[category];
+    const thisPrompt = thisCategoryPrompts[Math.floor(Math.random()*thisCategoryPrompts.length)];
+    console.log(thisPrompt);
 }
